63edb60733cd55c727cd404cc796280b335d774e
marrcos garofani <marrcosgarofani2305@gmail.com>
2026-02-03 14:16:19 -0300

    prueba pwa

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,23 @@
-const CACHE_NAME = "em-bebidas-v1";
+const CACHE_NAME = "em-pwa-v2";
+
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/tienda.html",
-  "/css/custom.css",
-  "/js/productos.js",
-  "/js/carrito.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./tienda.html",
+  "./css/custom.css",
+  "./js/productos.js",
+  "./js/carrito.js",
+  "./manifest.json"
 ];
 
-// Instalar
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activar
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-});
-
-// Fetch (offline)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
